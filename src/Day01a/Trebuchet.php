8602c9dbf4e9033330e535fa68719c2da1115617
Matthew Knowles <matthewyknowles@gmail.com>
2023-12-01 12:31:22 -0500

    halfway

--- src/Day01a/Trebuchet.php
+++ src/Day01a/Trebuchet.php
@@ -7,6 +7,17 @@ class Trebuchet
 {
     public function determineCalibrationValue(string $calibrationDocument): int
     {
-        return 11;
+        $onlyDigits = preg_replace('~\D~', '', $calibrationDocument);
+        $digitsLength = strlen($onlyDigits);
+        if ($digitsLength > 2) {
+            $firstDigit = $onlyDigits[0];
+            $lastDigit = $onlyDigits[$digitsLength-1];
+            return $firstDigit*10 + $lastDigit;
+        }
+        if ($digitsLength === 2) {
+            return (int) $onlyDigits;
+        }
+        $singleDigit = (int) $onlyDigits;
+        return $singleDigit*10 + $singleDigit;
     }
 }
--- tests/Day01a/TrebuchetTests.php
+++ tests/Day01a/TrebuchetTests.php
@@ -7,12 +7,57 @@ use PHPUnit\Framework\TestCase;
 
 class TrebuchetTests extends TestCase
 {
-    public function testOne(): void {
+    public function testSingleNumber(): void {
         $trebuchet = new Trebuchet();
-        $calibrationDocument = "1";
+        $calibrationDocument = '1';
 
         $result = $trebuchet->determineCalibrationValue($calibrationDocument);
 
-        $this->assertSame(11, $result);
+        static::assertSame(11, $result);
+    }
+
+    public function testDifferentSingleNumber(): void {
+        $trebuchet = new Trebuchet();
+        $calibrationDocument = '2';
+
+        $result = $trebuchet->determineCalibrationValue($calibrationDocument);
+
+        static::assertSame(22, $result);
+    }
+    public function testWithLetters(): void {
+        $trebuchet = new Trebuchet();
+        $calibrationDocument = 'd2fdsaf';
+
+        $result = $trebuchet->determineCalibrationValue($calibrationDocument);
+
+        static::assertSame(22, $result);
+    }
+    public function testWithTwoDigits(): void {
+        $trebuchet = new Trebuchet();
+        $calibrationDocument = 'd2fdsa3f';
+
+        $result = $trebuchet->determineCalibrationValue($calibrationDocument);
+
+        static::assertSame(23, $result);
+    }
+
+    public function testWithThreeDigits(): void {
+        $trebuchet = new Trebuchet();
+        $calibrationDocument = 'd2fdsa3f5tr';
+
+        $result = $trebuchet->determineCalibrationValue($calibrationDocument);
+
+        static::assertSame(25, $result);
+    }
+
+    public function testWithMultipleLines(): void {
+        $trebuchet = new Trebuchet();
+        $calibrationDocument =
+            '1
+            2';
+
+        $result = $trebuchet->determineCalibrationValue($calibrationDocument);
+
+        static::assertSame(33, $result);
     }
 }
