ea40e56fc995309e3dbaf3b3b2fa111a85ab21e0
Matthew Knowles <matthewyknowles@gmail.com>
2023-12-02 07:50:30 -0500

    better solution 🤷‍♂️

--- src/Day01a/Trebuchet.php
+++ src/Day01a/Trebuchet.php
@@ -5,24 +5,16 @@ namespace Day01a;
 
 class Trebuchet
 {
+    public function determineCalibrationValueWithEnglishNumbers(string $calibrationDocument): int
+    {
+        $calibrationDocumentTranslatesOnes = $this->replaceEnglishNumbersWithArabicNumbers($calibrationDocument);
+        return $this->determineCalibrationValue($calibrationDocumentTranslatesOnes);
+    }
+
     public function determineCalibrationValue(string $calibrationDocument): int
     {
         $lines = explode("\n", $calibrationDocument);
         return array_reduce($lines, fn(int $total, string $line) => $total + $this->calculateLineTotal($line), 0);
-    }
-
-    public function determineCalibrationValueWithEnglishNumbers(string $calibrationDocument): int
-    {
-        $calibrationDocumentTranslatesOnes = str_replace('one', 'one1one', $calibrationDocument);
-        $calibrationDocumentTranslatesTwos = str_replace('two', 'two2two', $calibrationDocumentTranslatesOnes);
-        $calibrationDocumentTranslatesThrees = str_replace('three', 'three3three', $calibrationDocumentTranslatesTwos);
-        $calibrationDocumentTranslatesFours = str_replace('four', 'four4four', $calibrationDocumentTranslatesThrees);
-        $calibrationDocumentTranslatesFives = str_replace('five', 'five5five', $calibrationDocumentTranslatesFours);
-        $calibrationDocumentTranslatesSixes = str_replace('six', 'six6six', $calibrationDocumentTranslatesFives);
-        $calibrationDocumentTranslatesSevens = str_replace('seven', 'seven7seven', $calibrationDocumentTranslatesSixes);
-        $calibrationDocumentTranslatesEights = str_replace('eight', 'eight8eight', $calibrationDocumentTranslatesSevens);
-        $calibrationDocumentTranslatesNines = str_replace('nine', 'nine9nine', $calibrationDocumentTranslatesEights);
-        return $this->determineCalibrationValue($calibrationDocumentTranslatesNines);
     }
 
     public function calculateLineTotal(string $line): int
@@ -31,5 +23,12 @@ class Trebuchet
         $firstDigitInTheTensPlace = $onlyDigits[0] * 10;
         $lastDigit = $onlyDigits[strlen($onlyDigits) - 1];
         return $firstDigitInTheTensPlace + $lastDigit;
+    }
+
+    public function replaceEnglishNumbersWithArabicNumbers(string $calibrationDocument): string|array|null
+    {
+        return preg_replace(['/one/', '/two/', '/three/', '/four/', '/five/', '/six/', '/seven/', '/eight/', '/nine/'],
+            ['one1one', 'two2two', 'three3three', 'four4four', 'five5five', 'six6six', 'seven7seven', 'eight8eight', 'nine9nine'],
+            $calibrationDocument);
     }
 }
