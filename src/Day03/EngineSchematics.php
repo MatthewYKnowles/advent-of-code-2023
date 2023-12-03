5805253b8c45564496619318768880df7b7c0c3b
Matthew Knowles <matthewyknowles@gmail.com>
2023-12-03 11:00:14 -0500

    All the if statements

--- src/Day03/EngineSchematics.php
+++ src/Day03/EngineSchematics.php
@@ -7,30 +7,67 @@ class EngineSchematics
 {
     public function determineSumOfPartNumbers(string $engineSchematics): int
     {
-        $lineCharacters = str_split($engineSchematics);
+        $games = explode("\n", $engineSchematics);
         $validNumbers = [];
-        $maybeValidNumber = '';
-        for ($x = 0; $x < count($lineCharacters); $x++) {
-            $character = $lineCharacters[$x];
-            if ($this->characterIsADigit($character)) {
-                $maybeValidNumber .= $character;
-                if($x + 1 === count($lineCharacters)){
-                    $validNumbers[] = (int) $maybeValidNumber;
+        for ($x = 0; $x < count($games); $x++) {
+            $lineCharacters = str_split($games[$x]);
+            $maybeValidNumber = '';
+            for ($y = 0; $y < count($lineCharacters); $y++) {
+                $character = $lineCharacters[$y];
+                if ($this->characterIsADigit($character)) {
+                    $maybeValidNumber .= $character;
                 }
-            }
-            if (!$this->characterIsADigit($character)) {
-                $hasSymbolNeighbor = $character !== '.';
-                $indexBeforeNumberStart = $x - strlen($maybeValidNumber) - 1;
-                if ($indexBeforeNumberStart >= 0) {
-                    $previousCharacter = $lineCharacters[$indexBeforeNumberStart];
-                    if ($previousCharacter !== '.') {
-                        $hasSymbolNeighbor = true;
+                if (!$this->characterIsADigit($character)) {
+                    $indexBeforeNumberStart = $y - strlen($maybeValidNumber) - 1;
+                    $hasSymbolNeighbor = $this->isSpecialChacter($character);
+                    if ($indexBeforeNumberStart >= 0) {
+                        $previousCharacter = $lineCharacters[$indexBeforeNumberStart];
+                        if ($this->isSpecialChacter($previousCharacter)) {
+                            $hasSymbolNeighbor = true;
+                        }
                     }
+                    if ($x + 1 < count($games)) {
+                        $nextGame = $games[$x+1];
+                        if ($this->isSpecialChacter($nextGame[$y])){
+                            $hasSymbolNeighbor = true;
+                        }
+                        $indexBeforeNumberStart = $y - strlen($maybeValidNumber);
+                        if ($indexBeforeNumberStart >= 0){
+                            if ($this->isSpecialChacter($nextGame[$indexBeforeNumberStart])){
+                                $hasSymbolNeighbor = true;
+                            }
+                        }
+                    }
+                    if ($hasSymbolNeighbor) {
+                        $validNumbers[] = (int) $maybeValidNumber;
+                    }
+                    $maybeValidNumber = '';
                 }
-                if ($hasSymbolNeighbor) {
-                    $validNumbers[] = (int) $maybeValidNumber;
+                if ($y + 1 === count($lineCharacters)) {
+                    $indexBeforeNumberStart = $y - strlen($maybeValidNumber);
+                    $hasSymbolNeighbor = $this->isSpecialChacter($character);
+                    if ($indexBeforeNumberStart >= 0) {
+                        $previousCharacter = $lineCharacters[$indexBeforeNumberStart];
+                        if ($this->isSpecialChacter($previousCharacter)) {
+                            $hasSymbolNeighbor = true;
+                        }
+                    }
+                    if ($x + 1 < count($games)) {
+                        $nextGame = $games[$x+1];
+                        if ($this->isSpecialChacter($nextGame[$y])){
+                            $hasSymbolNeighbor = true;
+                        }
+                        if ($indexBeforeNumberStart >= 0){
+                            if ($this->isSpecialChacter($nextGame[$indexBeforeNumberStart])){
+                                $hasSymbolNeighbor = true;
+                            }
+                        }
+                    }
+                    if ($hasSymbolNeighbor) {
+                        $validNumbers[] = (int) $maybeValidNumber;
+                    }
+                    $maybeValidNumber = '';
                 }
-                $maybeValidNumber = '';
             }
         }
         return array_sum($validNumbers);
@@ -39,5 +76,10 @@ class EngineSchematics
     public function characterIsADigit(mixed $character): int|false
     {
         return preg_match('/\d/', $character);
+    }
+
+    public function isSpecialChacter(mixed $character): bool
+    {
+        return $character !== '.' && !$this->characterIsADigit($character);
     }
 }
